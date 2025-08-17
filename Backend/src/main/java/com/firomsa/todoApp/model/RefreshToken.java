7f5c25a2948fb444b55191fdb761a62eff76072c
firomsa <firomsassf@gmail.com>
2025-08-17 23:41:35 +0300

    feat: added neccesary database models for authentication

---
+++ Backend/src/main/java/com/firomsa/todoApp/model/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.firomsa.todoApp.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "refresh_tokens")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class RefreshToken {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne()
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @NotNull
+    private LocalDateTime createdAt;
+    @NotNull
+
+    private LocalDateTime expiresAt;
+}
---
+++ Backend/src/main/java/com/firomsa/todoApp/model/User.java
@@ -0,0 +1,79 @@
+package com.firomsa.todoApp.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @Builder.Default
+    private boolean active = Boolean.TRUE;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", referencedColumnName = "id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    @Builder.Default
+    private List<RefreshToken> refreshTokens = new ArrayList<>();
+
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @NotNull
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.active;
+    }
+}
